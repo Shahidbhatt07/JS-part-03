f07ea4ae9c92a3237ce60e7d84598dcef2f6ccda
shahidbhatt07 <bhatshahid6542@gmail.com>
2022-11-28 17:53:28 +0530

    :lipstick: using Promise and setTimeout at a time

---
+++ 07.js
@@ -0,0 +1,19 @@
+//using Promise and setTimeout at a time
+
+function myPromise() {
+    return new Promise((resolve, reject)=> {
+        const value = true;
+        setTimeout(() => {
+            if (value) {
+                resolve();
+            } else {
+                reject();
+            }
+        }, 2000)
+    })
+}
+myPromise().then(() => {
+    console.log("resolved");
+}).catch(() =>{
+    console.log("rejected");
+})
