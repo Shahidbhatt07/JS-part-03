6e84a5980824b2311fefc0581dd982718af8d5a8
shahidbhatt07 <bhatshahid6542@gmail.com>
2022-11-29 19:27:38 +0530

    :recycle: AJAX

---
+++ 10.js
@@ -0,0 +1,29 @@
+//AJAX -> Asynchronous Javascript And Xml
+// BASIC THEORY
+
+// AJAX : asynchronous javascript and XML
+//XMl is data format but we dont use it now. we use JSON nowadays
+
+//Here we have to learn how to send HTTP request from browser(client) to server to create/update/send data
+//HTTP: Hyper Text Transfer Protocol
+//browser(client) and server communicate with each other wiith the help of HTTP
+
+// AJAX is
+// is a set of "web development techniques" 
+// using many web technologies on the "client-side "
+// to create asynchronous web applications. 
+
+
+// With Ajax, web applications can send and retrieve 
+// data from a server asynchronously (in the background) 
+// without interfering with the display and 
+// behaviour of the existing page
+
+// We don't use data in XML format anymore. 
+// we use JSON now. 
+
+
+// we have 3 most common ways to create and send request to server
+// 1.) xmlHTTPRequest (old way of doing)
+// 2.) fetch API (new way of doing)
+// 3.) axios (this is third party library)
